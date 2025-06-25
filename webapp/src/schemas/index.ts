33fe25b34828c19e8f0dafcaea5230f573073cb2
Artem Tarasenko <artem.t@macroactive.com>
2025-06-25 22:08:08 +0000

    fix: remove some unused data from schemas

--- webapp/src/schemas/index.ts
+++ webapp/src/schemas/index.ts
@@ -18,12 +18,8 @@ export const currentWeatherSchema = z
         main: z.object({
             temp: z.number(),
             feels_like: z.number(),
-            temp_min: z.number().optional(),
-            temp_max: z.number().optional(),
             pressure: z.number(),
             humidity: z.number(),
-            sea_level: z.number().optional(),
-            grnd_level: z.number().optional(),
         }),
         visibility: z.number().optional(),
         wind: z.object({
@@ -35,20 +31,8 @@ export const currentWeatherSchema = z
         clouds: z.object({
             all: z.number(),
         }),
-        dt: z.number(),
-        sys: z
-            .object({
-                type: z.number(),
-                id: z.number(),
-                country: z.string().optional(),
-                sunrise: z.number().optional(),
-                sunset: z.number().optional(),
-            })
-            .optional(),
-        timezone: z.number().optional(),
         id: z.number(),
         name: z.string(),
-        cod: z.number().optional(),
     })
     .describe('currentWeather');
 
@@ -56,20 +40,11 @@ export type ICurrentWeather = z.infer<typeof currentWeatherSchema>;
 
 export const cityInfoResponseSchema = z
     .object({
-        datasource: z
-            .object({
-                sourcename: z.string(),
-                attribution: z.string(),
-                license: z.string(),
-                url: z.string(),
-            })
-            .optional(),
         country: z.string().optional(),
         country_code: z.string().optional(),
         state: z.string().optional(),
         county: z.string().optional(),
         city: z.string(),
-        iso3166_2: z.string().optional(),
         lon: z.number(),
         lat: z.number(),
         state_code: z.string().optional(),
@@ -78,19 +53,6 @@ export const cityInfoResponseSchema = z
         address_line1: z.string(),
         address_line2: z.string(),
         category: z.string().optional(),
-        timezone: z
-            .object({
-                name: z.string(),
-                offset_STD: z.string(),
-                offset_STD_seconds: z.number(),
-                offset_DST: z.string(),
-                offset_DST_seconds: z.number(),
-                abbreviation_STD: z.string(),
-                abbreviation_DST: z.string(),
-            })
-            .optional(),
-        plus_code: z.string().optional(),
-        plus_code_short: z.string().optional(),
         rank: z
             .object({
                 importance: z.number().optional(),
@@ -101,14 +63,6 @@ export const cityInfoResponseSchema = z
             })
             .optional(),
         place_id: z.string(),
-        bbox: z
-            .object({
-                lon1: z.number().optional(),
-                lat1: z.number().optional(),
-                lon2: z.number().optional(),
-                lat2: z.number().optional(),
-            })
-            .optional(),
     })
     .describe('cityInfo');
 
