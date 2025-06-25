9674c723400c0a007cff9b72f65f65c0bfa093bf
Artem Tarasenko <artem.t@macroactive.com>
2025-06-25 20:47:22 +0000

    feat: update search component
    - make input controlled
    - add handler to set parent's state

--- webapp/src/components/Search.tsx
+++ webapp/src/components/Search.tsx
@@ -1,13 +1,21 @@
-type SearchProps = {};
+import { useState } from 'react';
 
-export default function Search(props: SearchProps) {
-    const {} = props;
+type SearchProps = {
+    onSelect: (city: string) => void;
+};
+
+export default function Search({ onSelect }: SearchProps) {
+    const [searchValue, setSearchValue] = useState<string>('');
+
+    function handleSubmitSearch() {
+        onSelect(searchValue);
+    }
 
     return (
         <div aria-labelledby="weather-search-label" className="weather-search-container w-full mb-8 relative">
             <form
                 className="weather-search flex w-full justify-center"
-                onSubmit={() => null}
+                onSubmit={handleSubmitSearch}
                 role="search"
                 aria-label="Search for a city"
             >
@@ -16,6 +24,8 @@ export default function Search(props: SearchProps) {
                     className="weather-search-input border border-gray-300 rounded-lg px-4 py-2 focus:outline-none text-zinc-100"
                     type="text"
                     placeholder="Search for a city..."
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    value={searchValue}
                 />
                 <button
                     className="weather-search-button bg-blue-500 text-white ml-2 px-4 py-2 rounded-r hover:bg-blue-600"
