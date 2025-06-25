ccf8f6eeef048fa9e01ab19218e12ef7c2e270f3
Artem Tarasenko <artem.t@macroactive.com>
2025-06-25 20:49:00 +0000

    Rework store to match schemas

--- webapp/src/store/index.ts
+++ webapp/src/store/index.ts
@@ -1,27 +1,25 @@
 import { create } from 'zustand';
-import type { ICurrentWeather } from '../schemas';
+import { type ICityInfo, type ICurrentWeather } from '../schemas';
 
 // State interface for weather app
 interface IWeatherAppState {
-    // Currently selected city or coordinates
-    city: Array<string>;
-    weather: Array<ICurrentWeather>;
+    // Currently selected cities
+    cities: Array<ICityInfo>;
     // Actions
-    addCity: (city: string) => void;
-    removeCity: (city: string) => void;
-    addWeather: (weatherInfo: ICurrentWeather) => void;
-    removeWeather: (weatherInfo: ICurrentWeather) => void;
+    addCity: (city: ICityInfo) => void;
+    removeCity: (place_id: string) => void;
+    addWeather: (weatherInfo: ICurrentWeather, place_id: string) => void;
 }
 
 // Zustand store for weather app state management
 export const useWeatherStore = create<IWeatherAppState>((set) => ({
     // Initial state
-    city: [],
-    weather: [],
+    cities: [],
     // Actions
-    addCity: (city: string) => set((state) => ({ city: [...state.city, city] })),
-    removeCity: (city: string) => set((state) => ({ city: state.city.filter((c) => c !== city) })),
-    addWeather: (infoToAdd: ICurrentWeather) => set((state) => ({ weather: [...state.weather, infoToAdd] })),
-    removeWeather: (infoToRemove: ICurrentWeather) =>
-        set((state) => ({ weather: state.weather.filter((info) => info.id !== infoToRemove.id) })),
+    addCity: (city: ICityInfo) => set((state) => ({ cities: [...state.cities, city] })),
+    removeCity: (place_id: string) => set((state) => ({ cities: state.cities.filter((c) => c.place_id !== place_id) })),
+    addWeather: (weatherInfo: ICurrentWeather, place_id: string) =>
+        set((state) => ({
+            cities: state.cities.map((city) => (city.place_id === place_id ? { ...city, weather: weatherInfo } : city)),
+        })),
 }));
