7c050cb390e8936e71c7aaba27df115aa69d3ae9
Artem Tarasenko <artem.t@macroactive.com>
2025-06-26 00:53:27 +0000

    fix: clean up state, remove unused action

--- webapp/src/store/index.ts
+++ webapp/src/store/index.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { type ICityInfo, type ICurrentWeather } from '../schemas';
+import { type ICityInfo } from '../schemas';
 
 // State interface for weather app
 interface IWeatherAppState {
@@ -8,18 +8,15 @@ interface IWeatherAppState {
     // Actions
     addCity: (city: ICityInfo) => void;
     removeCity: (place_id: string) => void;
-    addWeather: (weatherInfo: ICurrentWeather, place_id: string) => void;
 }
 
 // Zustand store for weather app state management
+// has data about fetched city search results and weather
+//plus several actions including "add weather"
 export const useWeatherStore = create<IWeatherAppState>((set) => ({
     // Initial state
     cities: [],
     // Actions
     addCity: (city: ICityInfo) => set((state) => ({ cities: [...state.cities, city] })),
     removeCity: (place_id: string) => set((state) => ({ cities: state.cities.filter((c) => c.place_id !== place_id) })),
-    addWeather: (weatherInfo: ICurrentWeather, place_id: string) =>
-        set((state) => ({
-            cities: state.cities.map((city) => (city.place_id === place_id ? { ...city, weather: weatherInfo } : city)),
-        })),
 }));
